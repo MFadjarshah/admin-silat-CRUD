ad67f74ec6451fbe3d4cdd9e89fcc7f3097408a2
MFadjarshah <shah1815@gmail.com>
2024-08-20 17:27:06 +0800

    create silat table in mysql

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -2,7 +2,7 @@ import "./list.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Table from "../../components/table/Table";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const List = () => {
   return (

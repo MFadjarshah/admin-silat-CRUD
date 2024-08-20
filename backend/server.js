941207a7c478e1633ebcd823a04d11f533b8d7c4
MFadjarshah <shah1815@gmail.com>
2024-08-21 00:52:33 +0800

    GET method failed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,27 @@ const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express();
+
+app.use(express.json());
+
 app.use(cors());
+
+// Connect to MySQL
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crud",
+});
+
+//GET method
+app.get("/table", (req, res) => {
+  const sql = "SELECT * FROM silat";
+  db.query(sql, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 app.listen(8081, () => {
   console.log("Listening...");
--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -2,9 +2,23 @@ import "./list.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Table from "../../components/table/Table";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
 // import { Link } from "react-router-dom";
 
 const List = () => {
+  // //To display the data
+  // const [silat, setSilat] = useState([]);
+
+  // //Fetch data from API database using Axios
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:8081/")
+  //     // .then((res) => console.log(res))
+  //     .then((res) => setSilat(res.data))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
   return (
     <div className="list">
       <Sidebar />

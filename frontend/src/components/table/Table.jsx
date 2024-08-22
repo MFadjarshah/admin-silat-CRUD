58ed66ffde533e3a3abd25af2841ab5131a5b6b3
MFadjarshah <shah1815@gmail.com>
2024-08-22 09:15:13 +0800

    Fetch using Axios

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -1,85 +1,22 @@
-// import "./table.scss";
-// import { DataGrid } from "@mui/x-data-grid";
-// import { memberColumns, memberRows } from "../../datatablesource";
-// import { Link } from "react-router-dom";
-// import React, { useState } from "react";
-
-// const Table = () => {
-//   // DELETE function
-//   const [data, setData] = useState(memberRows);
-
-//   const handleDelete = (id) => {
-//     setData(data.filter((item) => item.id !== id));
-//   };
-
-//   const actionColumn = [
-//     {
-//       field: "action",
-//       headerName: "Action",
-//       width: 200,
-//       renderCell: (params) => {
-//         return (
-//           <div className="cellAction">
-//             <Link to="/members/test" style={{ textDecoration: "none" }}>
-//               <div className="viewButton">View</div>
-//             </Link>
-//             <div
-//               className="deleteButton"
-//               onClick={() => handleDelete(params.row.id)}
-//             >
-//               Delete
-//             </div>
-//           </div>
-//         );
-//       },
-//     },
-//   ];
-
-//   return (
-//     <div className="table">
-//       <div className="tableTitle">
-//         List of Member
-//         <Link to="/members/new" className="link">
-//           Add New
-//         </Link>
-//       </div>
-//       <DataGrid
-//         rows={data}
-//         columns={memberColumns.concat(actionColumn)}
-//         initialState={{
-//           pagination: {
-//             paginationModel: { page: 0, pageSize: 10 },
-//           },
-//         }}
-//         pageSizeOptions={[5, 10]}
-//         checkboxSelection
-//       />
-//     </div>
-//   );
-// };
-
-// export default Table;
-
 import "./table.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { memberColumns, memberRows } from "../../datatablesource";
 import { Link } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Table = () => {
-  const [data, setData] = useState([]);
+  // DELETE function
+  const [data, setData] = useState(memberRows);
 
-  // Fetch data when the component mounts
+  // Fetch data from API database using Axios
   useEffect(() => {
-    const loadData = async () => {
-      const members = await memberRows();
-      setData(members);
-    };
-
-    loadData();
+    axios
+      .get("http://localhost:8081/")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
   }, []);
 
-  // DELETE function
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
   };
@@ -131,3 +68,75 @@ const Table = () => {
 };
 
 export default Table;
+
+// import "./table.scss";
+// import { DataGrid } from "@mui/x-data-grid";
+// import { memberColumns, memberRows } from "../../datatablesource";
+// import { Link } from "react-router-dom";
+// import React, { useState, useEffect } from "react";
+
+// const Table = () => {
+//   const [data, setData] = useState([]);
+
+//   // Fetch data when the component mounts
+//   useEffect(() => {
+//     const loadData = async () => {
+//       const members = await memberRows();
+//       setData(members);
+//     };
+
+//     loadData();
+//   }, []);
+
+//   // DELETE function
+//   const handleDelete = (id) => {
+//     setData(data.filter((item) => item.id !== id));
+//   };
+
+//   const actionColumn = [
+//     {
+//       field: "action",
+//       headerName: "Action",
+//       width: 200,
+//       renderCell: (params) => {
+//         return (
+//           <div className="cellAction">
+//             <Link to="/members/test" style={{ textDecoration: "none" }}>
+//               <div className="viewButton">View</div>
+//             </Link>
+//             <div
+//               className="deleteButton"
+//               onClick={() => handleDelete(params.row.id)}
+//             >
+//               Delete
+//             </div>
+//           </div>
+//         );
+//       },
+//     },
+//   ];
+
+//   return (
+//     <div className="table">
+//       <div className="tableTitle">
+//         List of Member
+//         <Link to="/members/new" className="link">
+//           Add New
+//         </Link>
+//       </div>
+//       <DataGrid
+//         rows={data}
+//         columns={memberColumns.concat(actionColumn)}
+//         initialState={{
+//           pagination: {
+//             paginationModel: { page: 0, pageSize: 10 },
+//           },
+//         }}
+//         pageSizeOptions={[5, 10]}
+//         checkboxSelection
+//       />
+//     </div>
+//   );
+// };
+
+// export default Table;

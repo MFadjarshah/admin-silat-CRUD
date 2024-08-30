9ba8f5bb848b57f1c952340e6b181d870452d2b9
MFadjarshah <shah1815@gmail.com>
2024-08-30 17:42:23 +0800

    update Edit

--- frontend/src/pages/edit/Edit.jsx
+++ frontend/src/pages/edit/Edit.jsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./edit.scss";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
 
 const Edit = () => {
+  const [name, setName] = useState("");
+
+  const { id } = useParams();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.put("http://localhost:8081" + id, { name });
+  };
+
   return (
     <div className="edit">
       <Sidebar />
       <div className="editContainer">
         <Navbar />
-        <div className="top">
-          <h1>Edit</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="top">
+            <div className="topContainer">
+              <h1 className="title">Update Information</h1>
+              <label htmlFor="">Name</label>
+              <input
+                type="text"
+                placeholder="Enter Name"
+                className="form-control"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <Link to="/members/view/:id" className="link">
+              <div className="updateButton">Update</div>
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
